37f292c23d2ae45da8afb46fe7a1f0e084cf140a
YEAMA <youssefemad99@hotmail.com>
2017-05-30 18:02:48 +0200

    Db name change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TODOapp')
+mongoose.connect('mongodb://YEAMA:0mnblkj.sa@ds151451.mlab.com:51451/nodetodoapp')
 
 module.exports.mongoose = mongoose;
