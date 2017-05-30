558dab4adc5acf0b69017c1686f1a810a3b0a26b
YEAMA <youssefemad99@hotmail.com>
2017-05-30 18:08:21 +0200

    Tweaks for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://YEAMA:0mnblkj.sa@ds151451.mlab.com:51451/nodetodoapp')
+mongoose.connect('mongodb://yeama:0mnblkj.sa@ds151451.mlab.com:51451/nodetodoapp')
 
 module.exports.mongoose = mongoose;
